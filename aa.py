2973e27c9f2eb5b8c7c994fec284e7ac5c8c0853
prateeksha-coder <prateeksha.codingal@gmail.com>
2025-11-02 12:47:46 +0530

    commit

--- aa.py
+++ aa.py
@@ -1,11 +1,11 @@
-#for ascending order sorting check
+#for descending order sorting check
 def checksorted(a):
    length = len(a)
    if length == 1 or length == 0:
        return True  
-   return a[0] <= a[1] and checksorted(a[1:])
+   return a[0] >= a[1] and checksorted(a[1:])
    
-a = [1,2,3,5,6,8,2] 
+a = [8,7,6,5,1,-1] 
 if checksorted(a):
    print("\nyes given array is sorted")
 else:
